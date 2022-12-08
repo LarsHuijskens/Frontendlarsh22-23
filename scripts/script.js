e955c44b01cb377ae87b5d781087e62c86d8ddf9
LarsHuijskens <90263426+LarsHuijskens@users.noreply.github.com>
2022-12-08 10:25:12 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@ console.log("hi");
 let deMenuButton = document.querySelector("header > button");
 let deSluitButton = document.querySelector("header nav  button");
 let deNav = document.querySelector("header nav:nth-of-type(2)");
+let deMain = document.querySelector("main")
+let deFooter = document.querySelector("footer")
 
 // event listeners menue button
 deMenuButton.addEventListener("click", toggleMenu);
@@ -13,4 +15,6 @@ deSluitButton.addEventListener("click", toggleMenu);
 // stap 3: Funtions menue
 function toggleMenu() {
   deNav.classList.toggle("open");
+  deMain.classList.toggle("open")
+  deFooter.classList.toggle("open")
 }
